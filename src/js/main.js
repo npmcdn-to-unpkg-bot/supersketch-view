509c36b10fb2072363950d094be2116ac9e8f04b
xlzqwerty1@gmail.com <xlzqwerty1@gmail.com>
2016-09-01 16:28:33 -0400

    added focusin focusout event for hamburger in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-var $hamburger = $(".hamburger");
-$hamburger.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
-    // Do something else, like open/close menu
+$(document).ready(function() {
+    // Set up hamburger
+    var $hamburger = $(".hamburger");
+    $hamburger.on("focusin", function(e) {
+        $hamburger.addClass("is-active");
+    });
+    $hamburger.on("focusout", function(e) {
+        $hamburger.removeClass("is-active");
+    });
+
 });
